5b409997764f57ff78bbb1bf3e8bcfbfa8e75585
Matthew Callis <matthew.callis@gmail.com>
2020-12-30 22:25:01 -0800

    Add simple types

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/**
+ * Uttori Document
+ *
+ * @property {string} [content=''] - Document Data
+ * @property {string} [slug=''] - Document URL Identifier
+ * @property {string} [title=''] -  - Document Name
+ * @property {string} [excerpt=''] - Preview of the content
+ * @property {string} [html=''] - Rendered HTML version of the content
+ * @property {string} [language=''] - Language of the document
+ * @property {string[]} [tags=[]] - Collection of tags to describe the document
+ * @property {object} [customData={}] - Any custom data
+ * @property {Date} [createDate] - Date & Time Created
+ * @property {Date} [updateDate] - Date & Time Last Updated, should default to the createDate
+ * @class
+ */
 class Document {
   constructor() {
     this.content = '';
---
+++ types/index.d.ts
@@ -0,0 +1,15 @@
+declare module "index" {
+    export = Document;
+    class Document {
+        content: string;
+        createDate: any;
+        customData: {};
+        excerpt: string;
+        html: string;
+        language: string;
+        slug: string;
+        tags: any[];
+        title: string;
+        updateDate: any;
+    }
+}
